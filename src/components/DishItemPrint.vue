2250c5ebb3ab957a8be71bce8283f2fa1e9a66aa
Ella Wirstad <ella.wg@hotmail.com>
2019-03-10 21:08:19 +0100

    improve printout page

--- src/components/DishItemPrint.vue
+++ src/components/DishItemPrint.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="item">
-      <div class="dish-item-print">
-        <img :src="url + dish.image">
-        <p>{{ dish.title }}</p>
-      </div>
+    <div class="dish-item-print">
+      <img :src="url + newDish.image">
+      <p>{{ newDish.title }}</p>
+      <p>{{ newDish.instructions }}</p>
+    </div>
   </div>
 </template>
 
@@ -12,8 +13,23 @@ export default {
   props: ["model", "dish"],
   data() {
     return {
-      url: "https://spoonacular.com/recipeImages/"
+      url: "https://spoonacular.com/recipeImages/",
+      newDish: {}
     };
+  },
+  mounted() {
+    // when data is retrieved we update it's properties
+    // this will cause the component to re-render
+    this.model
+      .getDish(this.dish.id)
+      .then(dish => {
+        this.status = "LOADED";
+        this.newDish = dish;
+      })
+      .catch(() => {
+        this.status = "ERROR";
+      });
+    this.model.addObserver(this);
   }
 };
 </script>
