e149e757fa0d55aa2f4304d4ba9cd6f9f8109253
lindacnatt <linda.cnattingius@gmail.com>
2019-03-20 22:50:18 +0100

    add hamburger

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,43 +1,67 @@
 <template>
   <div class="sidebar">
     <h3>This is the sidebar</h3>
-    <p>
-      People:
-      <input
-        type="number"
-        :value="numberOfGuests"
-        @input="onDidChangeNumberOfGuests"
-        @change="onDidChangeNumberOfGuests"
-      >
-      <br>
-      Total number of guests: {{ numberOfGuests }}
-    </p>
-    <table>
-      <tr>
-        <th>Dish Name</th>
-        <th>Cost</th>
-      </tr>
-      <tr v-for="dish in menu" :id="dish.id" :key="dish.id">
-        <td>{{dish.title}}</td>
-        <td>{{ numberOfGuests }} SEK</td>
-      </tr>
-    </table>
-    <p>Total price:</p>
-    <button
-      class="button"
-      @click="$router.push({ name: 'Overview', params: { menu: menu }})"
-    >Confirm Dinner</button>
+    <nav class="navbar is-light" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <p class="text">My dinner</p>
+        <a
+          role="button"
+          class="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          @click="isOpen = !isOpen"
+          v-bind:class="{'is-active': isOpen}"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div class="navbar-menu" v-bind:class="{'is-active': isOpen}">
+        <div class="navbar-end">
+          <div class="navbar-item is-tab">
+            <p>
+              People:
+              <input
+                type="number"
+                :value="numberOfGuests"
+                @input="onDidChangeNumberOfGuests"
+                @change="onDidChangeNumberOfGuests"
+              >
+              <br>
+              Total number of guests: {{ numberOfGuests }}
+            </p>
+            <table>
+              <tr>
+                <th>Dish Name</th>
+                <th>Cost</th>
+              </tr>
+              <tr v-for="dish in menu" :id="dish.id" :key="dish.id">
+                <td>{{dish.title}}</td>
+                <td>{{ numberOfGuests }} SEK</td>
+              </tr>
+            </table>
+            <p>Total price:</p>
+            <button
+              class="button"
+              @click="$router.push({ name: 'Overview', params: { menu: menu }})"
+            >Confirm Dinner</button>
+          </div>
+        </div>
+      </div>
+    </nav>
   </div>
 </template>
 
 <script>
 export default {
   props: ["model"],
-    // we define and initalise the data we want to use and modify in the component
+  // we define and initalise the data we want to use and modify in the component
   data() {
     return {
       numberOfGuests: this.model.getNumberOfGuests(),
-      menu: this.model.getMenu()
+      menu: this.model.getMenu(),
+      isOpen: false
     };
   },
   created() {
@@ -54,8 +78,7 @@ export default {
     update(model, changeDetails) {
       if (changeDetails === "numberOfGuests") {
         this.numberOfGuests = this.model.getNumberOfGuests();
-        localStorage.setItem('numberOfGuests', this.numberOfGuests);
-
+        localStorage.setItem("numberOfGuests", this.numberOfGuests);
       }
       if (changeDetails === "addDishToMenu") {
         this.menu = this.model.getMenu();
@@ -76,12 +99,40 @@ export default {
   max-width: 25vw;
   padding: 0 2% 0 2%;
 }
+.navbar{
+  display: flex;
+  flex-direction: column;
+}
+.navbar-item{
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+.navbar-item > * {
+  padding-bottom:10px;
+}
+.navbar-end{
+ margin:5px;
+}
+
 
 th {
   text-align: left;
 }
-
 td {
-  max-width: 300px;
+  max-width: 150px;
+  margin-right:5px;
+}
+.text{
+  margin:10px;
+}
+@media screen and (max-width: 750px){
+
+  td{
+    width:100%;
+  }
+  .navbar{
+    width:90vw;
+  }
 }
 </style>
